4f14b6725a17fca97344769a55718e43052fd5a6
beats0 <2047155291@qq.com>
2018-12-31 22:48:32 +0800

    update v3

--- Booru-Selector-Downloader/main.js
+++ Booru-Selector-Downloader/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru-Selector-Downloader
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  selector the pictures, consolog which you want and download the pictures online or use Node.js
 // @author       Beats0
 // @match          *://yande.re/post*
@@ -12,7 +12,10 @@
 // @include     *://konachan.net/*
 // @include     *://konachan.com/*
 // @include     *://danbooru.donmai.us/*
-// @grant        none
+// @grant        GM_download
+// @grant        GM_info
+// @grant       GM.download
+// @grant       GM.info
 // ==/UserScript==
 
 (function () {
@@ -23,21 +26,42 @@
     var REyandeResult = REyande.test(originUrl);
     var REkonachanResult = REkonachan.test(originUrl);
     var REdanbooruResult = REdanbooru.test(originUrl);
-    if (REyandeResult == true) {
+    if (REyandeResult === true) {
         yande();
-    } else if (REkonachanResult == true) {
+    } else if (REkonachanResult === true) {
         konachan();
-    } else if (REdanbooruResult == true) {
+    } else if (REdanbooruResult === true) {
         danbooru();
     }
+
 
     function yande() {
         var main_menu = document.getElementById("main-menu");
         var confBord = document.createElement('li');
-        confBord.setAttribute('class', 'static');
-        confBord.innerHTML = '<a href="#">Download</a> <a class="submenu-button" href="#">■</a> <ul class="submenu" style="display: block;"> <li><a class="help-item post current-menu ManagementButton" id="ButtonSelectAll" onclick="javascript:UpdateBatchCount();" style="color: #ee8887;display: inline;cursor:pointer;">Select All</a> </li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson()">logJson</a></li></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson();Yande.sample()">sample version</a></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="directlinkArr();Yande.larger()">larger version</a></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson();Yande.original()">original version</a></ul>';
-        main_menu.firstElementChild.appendChild(confBord);
         var posts = document.getElementById('post-list-posts');
+        if (!posts) return
+        confBord.setAttribute('class', 'static');
+        confBord.innerHTML = `<a href="#">Download</a>
+ <a class="submenu-button" href="#">■</a> 
+ <ul class="submenu" style="display: block;">
+  <li>
+  <a class="help-item post current-menu ManagementButton" id="ButtonSelectAll" onclick="javascript:UpdateBatchCount();" style="color: #ee8887;display: inline;cursor:pointer;">Select All</a>
+   </li>
+   <li>
+   <a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson()">logJson</a>
+   </li>
+   </li>
+   <li>
+   <a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson();Yande.sample()">sample version</a>
+   </li>
+   <li>
+   <a style="color: #ee8887;display: inline;cursor:pointer;" onclick="directlinkArr();Yande.larger()">larger version</a>
+   </li>
+   <li>
+   <a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson();Yande.original()">original version</a>
+   </li>
+   </ul>`;
+        main_menu.firstElementChild.appendChild(confBord);
         var postsItems = posts.querySelectorAll('li');
         for (var i = 0; i < postsItems.length; i++) {
             postsItems[i].classList.add('imgItem');
@@ -56,10 +80,11 @@
     function konachan() {
         var main_menu = document.getElementById("main-menu");
         var confBord = document.createElement('li');
+        var posts = document.getElementById('post-list-posts');
+        if (!posts) return
         confBord.setAttribute('class', 'static');
         confBord.innerHTML = '<a href="#">Download</a> <a class="submenu-button" href="#">■</a> <ul class="submenu" style="display: block;"> <li><a class="help-item post current-menu ManagementButton" id="ButtonSelectAll" onclick="javascript:UpdateBatchCount();" style="color: #ee8887;display: inline;cursor:pointer;">Select All</a> </li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson()">logJson</a></li></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="directlinkArr();Konachan.larger()">larger version</a></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logJson();Konachan.original()">original for PNG</a></li></ul>';
         main_menu.firstElementChild.appendChild(confBord);
-        var posts = document.getElementById('post-list-posts');
         var postsItems = posts.querySelectorAll('li');
         for (var i = 0; i < postsItems.length; i++) {
             postsItems[i].classList.add('imgItem');
@@ -78,10 +103,11 @@
     function danbooru() {
         var main_menu = document.getElementById('nav');
         var confBord = document.createElement('li');
+        var posts = document.getElementById('posts');
+        if (!posts) return;
         confBord.setAttribute('class', 'static');
         confBord.innerHTML = '<a href="#">Download</a> <a class="submenu-button" href="#">■</a> <ul class="submenu" style="display: block;"> <li><a class="help-item post current-menu ManagementButton" id="ButtonSelectAll" onclick="javascript:UpdateBatchCount();" style="color: #ee8887;display: inline;cursor:pointer;">Select All</a> </li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logDanbooruJson()">logJson</a></li></li><li><a style="color: #ee8887;display: inline;cursor:pointer;" onclick="logDanbooruJson();Danbooru.original()">download original(for jpg and png)</a></li></ul>';
         main_menu.firstElementChild.appendChild(confBord);
-        var posts = document.getElementById('posts');
         var postsItems = posts.querySelectorAll('article');
         for (var i = 0; i < postsItems.length; i++) {
             postsItems[i].classList.add('imgItem');
@@ -106,6 +132,119 @@
     }
 
     loadCssCode('ul#post-list-posts li {float:none}.imgItem:hover, .imgItem:focus { outline: 1px solid Highlight;outline: 1px auto -webkit-focus-ring-colo; } .imgItemChecked { outline: 1px solid Highlight;outline: 1px auto -webkit-focus-ring-colo; } article.post-preview { float:none;}');
+
+    var removeFav = document.getElementById('remove-from-favs')
+    var addFav = document.getElementById('add-to-favs')
+
+    var flag = false
+
+    // sample version
+    function downloadSample() {
+        for (let i = 0; i < pUrls.length; i++) {
+            let url = ''
+            if (REyandeResult) {
+                url = 'https://files.yande.re/sample/' + pUrls[i].md5 + '/' + pUrls[i].id + '.jpg';
+                GMDownload(url, pUrls[i].id)
+            } else if (REkonachanResult) {
+                url = 'https://konachan.com/sample/' + pUrls[i].md5 + '/' + pUrls[i].id + '.jpg';
+                GMDownload(url, pUrls[i].id)
+            }
+        }
+    }
+
+    // larger version
+    let largerArr = Array.from(document.querySelectorAll('.largeimg')).map((pic, index) => pic.href)
+    let unMathArr = []
+
+    function downloadLarger() {
+        for (let i = 0; i < pUrls.length; i++) {
+            let re = new RegExp(pUrls[i].md5, 'i')
+            largerArr.map((pic, index) => {
+                if (re.test(pic)) {
+                    GMDownload(pic, pUrls[i].md5)
+                } else {
+                    unMathArr = [...unMathArr, `${pUrls[i].id}/${pUrls[i].md5}`]
+                }
+            })
+        }
+        unMathArr.map((pic, index) => {
+            console.warn(`${pic} dosen't match`)
+        })
+    }
+
+    // GM downloader
+    function GMDownload(url, name) {
+        var arg = {
+            url,
+            name: name + '.jpg',
+            onprogress: downloadProgress
+        }
+        console.log(arg)
+        GM_download(arg)
+    }
+
+    // press key 'Left' or 'Right' to paginate
+    var pageRight = document.querySelector('#paginator > div > a.next_page')
+    var pageLeft = document.querySelector('#paginator > div > a.previous_page')
+
+    function GMhandler(e) {
+        // sample
+        if (e.key === 'z') {
+            flag = true
+            logJson()
+            downloadSample()
+        }
+        // larger
+        if (e.key === 'x') {
+            flag = true
+            logJson()
+            downloadLarger()
+        }
+        // change fav
+        if (e.key === 'c' && removeFav && addFav) {
+            var addLink = addFav.firstElementChild
+            var removeLink = removeFav.firstElementChild
+            removeFav.style.display === 'none' ? addLink.click() : removeLink.click()
+        }
+        // paginate
+        if (e.key === 'd' && pageRight) {
+            pageRight.click()
+        }
+        if (e.key === 'a' && pageLeft) {
+            pageLeft.click()
+        }
+    }
+
+    window.addEventListener('keydown', GMhandler, false)
+
+    // create progress
+    var body = document.getElementsByTagName('body')[0];
+    var progressEl = document.createElement('div');
+    progressEl.innerHTML = `
+    <div id='stripe' style="
+    position: fixed;
+    height: 2px;
+    width: 0;
+    margin: 0 -3em;
+    top: 0;
+    background: #ee8887;
+    box-shadow: 0 0 10px rgba(249,144,141,0.7);
+    transition: width .4s ease;
+"></div>`
+    body.appendChild(progressEl)
+
+    // downloader progress
+    function downloadProgress(xhr) {
+        try {
+            if (!xhr.lengthComputable)
+                return;
+            var stripe = document.getElementById('stripe'),
+                width = Math.floor(xhr.loaded / xhr.total * 100);
+            width === 100 ? stripe.style.width = 0 : stripe.style.width = `${width}%`
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
     // myselect.js
     var HeadEl = document.getElementsByTagName('head').item(0);
