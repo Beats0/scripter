c0061def17d9210127d7a7dd2b3a2d43420f145b
beats0 <2047155291@qq.com>
2019-02-03 22:37:11 +0800

    yande.re bd

---
+++ yande.re_db/gmDownloader.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         yande.re_db
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  yande.re_db downloader by Beats0
+// @author       Beats0
+// @match        *://beats0.github.io/scripter/*
+// @match        *://localhost/*
+// @grant        GM_download
+// @grant        GM_info
+// @grant        GM.download
+// @grant        GM.info
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const re = /([a-fA-F0-9]{32})/;
+
+    function handleDownload(e) {
+        if (e.target.id === 'download') {
+            const db = JSON.parse(localStorage.getItem('yande.re_db'))
+            for (let i = 0; i < db.length; i++) {
+                GMDownload(db[i], db[i].match(re)[0])
+            }
+        }
+    }
+
+    // GM downloader
+    function GMDownload(url, name) {
+        var arg = {
+            url: `https://steamuserimages-a.akamaihd.net/ugc/${url}`,
+            name: `${name}.jpg`,
+            onprogress: downloadProgress
+        }
+        console.log(arg)
+        GM_download(arg)
+    }
+
+    // downloader progress
+    function downloadProgress(xhr) {
+        try {
+            if (!xhr.lengthComputable) return;
+            var stripe = document.getElementById('stripe'),
+                width = Math.floor(xhr.loaded / xhr.total * 100);
+            width === 100 ? stripe.style.width = 0 : stripe.style.width = `${width}%`
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // create progress
+    var body = document.getElementsByTagName('body')[0];
+    var progressEl = document.createElement('div');
+    progressEl.innerHTML = `
+    <div id='stripe' style="
+    position: fixed;
+    height: 2px;
+    width: 0;
+    margin: 0 -3em;
+    top: 0;
+    background: #ee8887;
+    box-shadow: 0 0 10px rgba(249,144,141,0.7);
+    transition: width .4s ease;
+"></div>`
+    body.appendChild(progressEl)
+
+    window.addEventListener('click', handleDownload, false)
+})();
